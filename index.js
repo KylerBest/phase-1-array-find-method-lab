73263a0b51823176a11ae3ac62e1e9ec919a257e
Kyler <kyler.b@live.com>
2021-11-03 20:22:32 -0500

    commit tree ac9e49ab5bc4d2acaa78437b1aae5288cc322872

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+function superbowlWin(records) {
+    const win = records.find(record => record.result === 'W');
+    if(win != undefined){
+        return win.year;
+    }else{
+        return;
+    }
+}
